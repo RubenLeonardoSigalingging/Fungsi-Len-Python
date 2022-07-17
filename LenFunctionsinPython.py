4f5b8e79b2610ca8708d86cd515d68c9d538a1da
Ruben Leonardo Sigalingging <106874389+RubenLeonardoSigalingging@users.noreply.github.com>
2022-07-17 19:38:42 +0700

    Add files via upload

---
+++ LenFunctionsinPython.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from os import system
+system("clear")
+
+
+MD5="ca0dbad92a874b2f69b549293387925e"
+MD2="811510e23bba45e7af8ada350046ca84"
+Base32="INUWOYLSMV2HIZI="
+Namaku="Ruben Leonardo Sigalingging"
+print(f"[!] The String Length Of {Namaku} Is: {len(Namaku)}")
+print(f"[!] The String Length Of {Base32} Is: {len(Base32)}")
+print(f"[!] The String Length Of {MD5} Is: {len(MD5)}")
+print(f"[!] The String Length Of {MD2} Is: {len(MD2)}")
